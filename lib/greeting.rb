887dde13d485b2e6e16e0aa1560773da8642cb5c
Arnold Su <arnoldtingsu@gmail.com>
2018-06-19 19:16:07 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -8,12 +8,12 @@ end
 
 
 def age_reply
-  puts "That's pretty old."
+  puts "#{age_reply}?That's pretty old."
   
 end
 
 
 def location_reply
-  puts "I know where that is!"
+  puts "#{location_reply}, really? I know where that is!"
   
 end
